0f654d5c9d53a5f1ea346d247268b8dd1a59bdbb
Damien McDonnell <damien@clickstart.ie>
2024-11-18 19:47:02 +0000

    Add HomepageHeader component

---
+++ components/HomepageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    subtitle: {
+      type: String,
+      default: 'Subtitle'
+    },
+    content: {
+      type: String,
+      default: 'Content'
+    }
+  })
+</script>
+
+<style scoped></style>
